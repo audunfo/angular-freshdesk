755f0106462b886ee0239d0fe725137c096a641e
Raymond Jelierse <raymond@shareworks.nl>
2014-10-08 12:59:07 +0200

    docs(api): add API documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,67 @@
+NG_DOCS={
+  "sections": {
+    "api": "API documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "freshdesk.$freshDesk",
+      "shortName": "freshdesk.$freshDesk",
+      "type": "object",
+      "moduleName": "freshdesk",
+      "shortDescription": "The wrapper service for the FreshDesk components.",
+      "keywords": "$freshchat $freshdesk $freshwidget api chat components enabled freshdesk function init initializes object service widget wrapper"
+    },
+    {
+      "section": "api",
+      "id": "freshdesk.$freshDeskProvider",
+      "shortName": "freshdesk.$freshDeskProvider",
+      "type": "object",
+      "moduleName": "freshdesk",
+      "shortDescription": "Utility service for the different FreshDesk components.",
+      "keywords": "$freshchatprovider $freshdeskprovider $freshwidgetprovider account api chat common component components configure enable enablechat enablewidget freshchat freshdesk freshwidget function internally link object portal provider service set setaccount setaccounturl settings support url utility widget"
+    },
+    {
+      "section": "api",
+      "id": "freshdesk.chat.$freshChat",
+      "shortName": "freshdesk.chat.$freshChat",
+      "type": "object",
+      "moduleName": "freshdesk.chat",
+      "shortDescription": "The $freshChat service is a wrapper to inject the FreshChat widget into the page.",
+      "keywords": "$document $freshchat $location $window allows alter api chat class freshchat freshdesk freshdesk-chat-freshchat-page inject labels object service translate widget wrapper"
+    },
+    {
+      "section": "api",
+      "id": "freshdesk.chat.$freshChatProvider",
+      "shortName": "freshdesk.chat.$freshChatProvider",
+      "type": "object",
+      "moduleName": "freshdesk.chat",
+      "shortDescription": "The provider for the $freshChat service. Allows boot-time configuration of the FreshChat widget.",
+      "keywords": "$freshchat $freshchatprovider account allows api boot-time chat client configuration credentials freshchat freshdesk function internally link object portal provider secret service set setaccount setaccounturl setcredentials support url widget"
+    },
+    {
+      "section": "api",
+      "id": "freshdesk.widget.$freshWidget",
+      "shortName": "freshdesk.widget.$freshWidget",
+      "type": "object",
+      "moduleName": "freshdesk.widget",
+      "keywords": "$document $freshwidget $location $q $window add address api destroy dom email emailaddress extra fields form freshdesk freshwidget function hide identify init initializes inject injects loaded object options opts pass promise re-initialize requester resolve script update widget"
+    },
+    {
+      "section": "api",
+      "id": "freshdesk.widget.$freshWidgetProvider",
+      "shortName": "freshdesk.widget.$freshWidgetProvider",
+      "type": "object",
+      "moduleName": "freshdesk.widget",
+      "keywords": "$freshwidgetprovider account active api environment file freshdesk freshwidget function internally javascript js library link multi-product object options opts override portal prod product set setaccount setaccounturl setoptions setproduct setscripturl support url widget"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api/freshdesk.$freshDesk",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
