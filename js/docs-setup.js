0f53e4ac5fd2c15c13a99e09412c112cff8d69aa
Raymond Jelierse <raymond@shareworks.nl>
2014-10-08 12:26:15 +0200

    Automated documentation build.

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -5,62 +5,90 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
-      "id": "freshdesk.$freshDesk",
-      "shortName": "freshdesk.$freshDesk",
-      "type": "object",
-      "moduleName": "freshdesk",
-      "shortDescription": "The wrapper service for the FreshDesk components.",
-      "keywords": "$freshchat $freshdesk $freshwidget api chat components enabled freshdesk function init initializes object service widget wrapper"
+      "id": "sw.freshdesk",
+      "shortName": "sw.freshdesk",
+      "type": "overview",
+      "moduleName": "sw.freshdesk",
+      "shortDescription": "The sw.freshdesk module adds integration of FreshDesk into AngularJS.",
+      "keywords": "adds angularjs api freshdesk http integration module overview sw"
     },
     {
       "section": "api",
-      "id": "freshdesk.$freshDeskProvider",
-      "shortName": "freshdesk.$freshDeskProvider",
-      "type": "object",
-      "moduleName": "freshdesk",
-      "shortDescription": "Utility service for the different FreshDesk components.",
-      "keywords": "$freshchatprovider $freshdeskprovider $freshwidgetprovider account api chat common component components configure enable enablechat enablewidget freshchat freshdesk freshwidget function internally link object portal provider service set setaccount setaccounturl settings support url utility widget"
+      "id": "sw.freshdesk-chat",
+      "shortName": "sw.freshdesk-chat",
+      "type": "overview",
+      "moduleName": "sw.freshdesk-chat",
+      "shortDescription": "This module adds FreshChat support to AngularJS.",
+      "keywords": "adds angularjs api freshchat freshdesk-chat module overview support sw"
     },
     {
       "section": "api",
-      "id": "freshdesk.chat.$freshChat",
-      "shortName": "freshdesk.chat.$freshChat",
+      "id": "sw.freshdesk-chat.$freshChat",
+      "shortName": "sw.freshdesk-chat.$freshChat",
       "type": "object",
-      "moduleName": "freshdesk.chat",
+      "moduleName": "sw.freshdesk-chat",
       "shortDescription": "The $freshChat service is a wrapper to inject the FreshChat widget into the page.",
-      "keywords": "$document $freshchat $location $window allows alter api chat class freshchat freshdesk freshdesk-chat-freshchat-page inject labels object service translate widget wrapper"
+      "keywords": "$document $freshchat $location $window allows alter api class freshchat freshdesk-chat inject labels object service sw sw-freshdesk-chat-freshchat-page translate widget wrapper"
     },
     {
       "section": "api",
-      "id": "freshdesk.chat.$freshChatProvider",
-      "shortName": "freshdesk.chat.$freshChatProvider",
+      "id": "sw.freshdesk-chat.$freshChatProvider",
+      "shortName": "sw.freshdesk-chat.$freshChatProvider",
       "type": "object",
-      "moduleName": "freshdesk.chat",
+      "moduleName": "sw.freshdesk-chat",
       "shortDescription": "The provider for the $freshChat service. Allows boot-time configuration of the FreshChat widget.",
-      "keywords": "$freshchat $freshchatprovider account allows api boot-time chat client configuration credentials freshchat freshdesk function internally link object portal provider secret service set setaccount setaccounturl setcredentials support url widget"
+      "keywords": "$freshchat $freshchatprovider account allows api boot-time client configuration credentials freshchat freshdesk freshdesk-chat function internally link object portal provider secret service set setaccount setaccounturl setcredentials support sw url widget"
     },
     {
       "section": "api",
-      "id": "freshdesk.widget.$freshWidget",
-      "shortName": "freshdesk.widget.$freshWidget",
-      "type": "object",
-      "moduleName": "freshdesk.widget",
-      "keywords": "$document $freshwidget $location $q $window add address api destroy dom email emailaddress extra fields form freshdesk freshwidget function hide identify init initializes inject injects loaded object options opts pass promise re-initialize requester resolve script update widget"
+      "id": "sw.freshdesk-widget",
+      "shortName": "sw.freshdesk-widget",
+      "type": "overview",
+      "moduleName": "sw.freshdesk-widget",
+      "shortDescription": "This module adds FreshWidget support to AngularJS.",
+      "keywords": "adds angularjs api freshdesk-widget freshwidget module overview support sw"
     },
     {
       "section": "api",
-      "id": "freshdesk.widget.$freshWidgetProvider",
-      "shortName": "freshdesk.widget.$freshWidgetProvider",
+      "id": "sw.freshdesk-widget.$freshWidget",
+      "shortName": "sw.freshdesk-widget.$freshWidget",
       "type": "object",
-      "moduleName": "freshdesk.widget",
-      "keywords": "$freshwidgetprovider account active api environment file freshdesk freshwidget function internally javascript js library link multi-product object options opts override portal prod product set setaccount setaccounturl setoptions setproduct setscripturl support url widget"
+      "moduleName": "sw.freshdesk-widget",
+      "keywords": "$document $freshwidget $location $q $window add addoptions address api destroy dom email emailaddress extra fields form freshdesk-widget freshwidget function hide identify init initializes injects loaded object options opts pass promise re-initialize requester resolve script sw update widget"
+    },
+    {
+      "section": "api",
+      "id": "sw.freshdesk-widget.$freshWidgetProvider",
+      "shortName": "sw.freshdesk-widget.$freshWidgetProvider",
+      "type": "object",
+      "moduleName": "sw.freshdesk-widget",
+      "keywords": "$freshwidgetprovider account active api environment file freshdesk freshdesk-widget freshwidget function internally javascript js library link multi-product object options opts override portal prod product set setaccount setaccounturl setoptions setproduct setscripturl support sw url widget"
+    },
+    {
+      "section": "api",
+      "id": "sw.freshdesk.$freshDesk",
+      "shortName": "sw.freshdesk.$freshDesk",
+      "type": "object",
+      "moduleName": "sw.freshdesk",
+      "shortDescription": "The wrapper service for the FreshDesk components.",
+      "keywords": "$freshchat $freshdesk $freshwidget api components enabled freshdesk freshdesk-chat freshdesk-widget function init initializes object service sw wrapper"
+    },
+    {
+      "section": "api",
+      "id": "sw.freshdesk.$freshDeskProvider",
+      "shortName": "sw.freshdesk.$freshDeskProvider",
+      "type": "object",
+      "moduleName": "sw.freshdesk",
+      "shortDescription": "Utility service for the different FreshDesk components.",
+      "keywords": "$freshchatprovider $freshdeskprovider $freshwidgetprovider account api common component components configure enable enablechat enablewidget freshchat freshdesk freshdesk-chat freshdesk-widget freshwidget function internally link object portal provider service set setaccount setaccounturl settings support sw url utility"
     }
   ],
   "apis": {
     "api": true
   },
   "html5Mode": false,
-  "startPage": "/api/freshdesk.$freshDesk",
+  "editExample": true,
+  "startPage": "/api",
   "scripts": [
     "angular.min.js"
   ]
